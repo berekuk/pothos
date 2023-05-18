c052411de6dcd4a7e3d92c83c0b111ead97351ba
Michael Hayes <michael@hayes.io>
2023-05-18 23:13:58 +1000

    Make default field nullability true by default

--- packages/core/src/builder.ts
+++ packages/core/src/builder.ts
@@ -76,7 +76,7 @@ export class SchemaBuilder<Types extends SchemaTypes> {
     string,
     {
       v3?: (
-        options: NormalizeSchemeBuilderOptions<SchemaTypes & { Defaults: 'v3' }>,
+        options: AddVersionedDefaultsToBuilderOptions<SchemaTypes, 'v3'>,
       ) => Partial<NormalizeSchemeBuilderOptions<SchemaTypes>>;
       v4?: undefined;
     }
@@ -95,12 +95,11 @@ export class SchemaBuilder<Types extends SchemaTypes> {
   constructor(options: PothosSchemaTypes.SchemaBuilderOptions<Types>) {
     this.options = [...SchemaBuilder.optionNormalizers.values()].reduce((opts, normalize) => {
       if (options.defaults && typeof normalize[options.defaults] === 'function') {
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
         return {
           ...opts,
-          ...normalize[options.defaults]!(
-            opts as NormalizeSchemeBuilderOptions<SchemaTypes & { Defaults: 'v3' }>,
-          ),
-        };
+          ...normalize[options.defaults]!(opts),
+        } as PothosSchemaTypes.SchemaBuilderOptions<Types>;
       }
 
       return opts;
@@ -113,7 +112,9 @@ export class SchemaBuilder<Types extends SchemaTypes> {
         options as {
           defaultFieldNullability?: boolean;
         }
-      ).defaultFieldNullability ?? false;
+      ).defaultFieldNullability ??
+      // eslint-disable-next-line no-unneeded-ternary
+      (options.defaults === 'v3' ? false : true);
 
     this.defaultInputFieldRequiredness =
       (
